01d6d9845b55fceff8fb82a8d0eca78b2d743a25
Fredrik Jeppsson <fredrik.m.jeppsson@gmail.com>
2020-12-11 11:09:06 +0100

    Added code that gets the quiz key from an environment variable.

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,7 +1,19 @@
 import static spark.Spark.*;
 
 public class Main {
+    private static String quizKey = Main.quizKeyFromEnv("quiz");
+
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
+
+    }
+
+    private static String quizKeyFromEnv(String varName) {
+        var key = System.getenv(varName);
+        if (key == null) {
+            System.err.println("Failed to load API key for quizapi.io");
+            System.exit(1);
+        }
+        return key;
     }
 }
