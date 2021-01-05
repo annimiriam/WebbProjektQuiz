017aaf623ad2abcfccd78b361615ce05656eeab1
moasvennesson <62596297+moasvennesson@users.noreply.github.com>
2021-01-05 19:59:31 +0100

    fixat all nav så rätt länkar

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ def php_quiz_page():
     return template("Quiz", name='php')
 
 @route('/javascript')
-def php_quiz_page():
+def javascript():
     return template("Quiz", name='javascript')
 
 @route('/wordpress')
 def wordpress_quiz_page():
     return template("Quiz", name='wordpress')
 
-run(host='localhost', port=8083, debug=True, reloader=True)
+run(host='localhost', port=8084, debug=True, reloader=True)
